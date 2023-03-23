c85405ebcbb019a1d1232a76168c3d2b3dce306e
Danika Gupta <danika@gprof.com>
2023-03-23 10:36:40 -0700

    Initial Commit

---
+++ detectCataract/detectCataractApp.swift
@@ -0,0 +1,17 @@
+//
+//  detectCataractApp.swift
+//  detectCataract
+//
+//  Created by Amit Gupta on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct detectCataractApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
